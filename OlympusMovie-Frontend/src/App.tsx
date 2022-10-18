914737f7372260e60100eab7e4829b4e7ce0b267
Jakub Nowak <jakubszymonnowak@gmail.com>
2022-10-18 17:28:33 +0200

    OM-18 Add background gradient and move padding to logo

--- OlympusMovie-Frontend/src/App.tsx
+++ OlympusMovie-Frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Nav  from './components/Nav/Nav';
+import Nav from './components/Nav/Nav';
 
 function App() {
     return (
-        <>
-            <Nav />
-            <h1>Test Hello World!</h1>
-        </>
+        < div style={{ backgroundImage: "linear-gradient(#ffb300, #ff4100)", height: "100vh" }}>
+            <>
+                <Nav />
+                <h1>Test Hello World!</h1>
+            </>
+        </div>
   );
 }
 
