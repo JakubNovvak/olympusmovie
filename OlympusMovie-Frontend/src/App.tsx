c388456c32136ba97181958843bbc2d81bdd44ab
Jakub Nowak <jakubszymonnowak@gmail.com>
2022-10-18 14:25:07 +0200

    OM-18 Change returned content of App function in App.tsx

--- OlympusMovie-Frontend/src/App.tsx
+++ OlympusMovie-Frontend/src/App.tsx
@@ -4,24 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olympus Movie
-        </p>
-        <p>
-                  filmy &nbsp; - &nbsp; recenzje &nbsp; - &nbsp; seriale
-        </p>
-        <a
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Coming soon
-        </a>
-      </header>
-    </div>
+    <h1>Test Hello World!</h1>
   );
 }
 
