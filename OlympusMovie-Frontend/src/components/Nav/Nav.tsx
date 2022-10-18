e587f496ce1438f04fee940470ad6c6613550f4a
Jakub Nowak <jakubszymonnowak@gmail.com>
2022-10-18 18:00:59 +0200

    Add search icon and "log in"/"register" options on navbar

--- OlympusMovie-Frontend/src/components/Nav/Nav.tsx
+++ OlympusMovie-Frontend/src/components/Nav/Nav.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+﻿import React from 'react';
 import styles from './Nav.module.css';
 import logo from '../../logo.svg';
+import searchIcon from '../../searchIcon.svg';
 
 const Nav: React.FC<{}> = () => {
     return (
@@ -8,6 +9,21 @@ const Nav: React.FC<{}> = () => {
             <div className={styles.logoContainer}>
                 <img src={logo} alt="logo"></img>
             </div>
+
+            <div className={styles.navOptions}>
+                <div>
+                    <a href=''>Zaloguj się</a>
+                </div>
+
+                <div>
+                    <a href=''>Zarejestruj się</a>
+                </div>
+
+                <div>
+                    <img src={searchIcon} alt="search"></img>
+                </div>
+            </div>
+
         </nav>
     )
 }
