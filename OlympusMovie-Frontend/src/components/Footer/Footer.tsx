83f0dbed21891a833b6228abfcf9ae19d0f60d13
Jakub Nowak <jakubszymonnowak@gmail.com>
2022-11-08 10:55:21 +0100

    OM-22 Implement the footer of the website # add basic content of the .tsx file

---
+++ OlympusMovie-Frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'; 
+import styles from './Footer.module.css';
+
+const Footer = ({ }) => {
+
+    return (
+
+        <>
+            Footer
+        </>
+        
+        
+        
+        );
+
+}
+
+
+export default Footer;
