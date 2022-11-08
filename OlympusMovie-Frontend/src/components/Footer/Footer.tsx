faca5668a54fa1a7e4e12b5b5487fc51a674d2f4
Jakub Nowak <jakubszymonnowak@gmail.com>
2022-11-08 12:04:29 +0100

    OM-22 Implement the footer of the website # Add basic content of the footer

--- OlympusMovie-Frontend/src/components/Footer/Footer.tsx
+++ OlympusMovie-Frontend/src/components/Footer/Footer.tsx
@@ -1,16 +1,30 @@
-import React from 'react'; 
+﻿import React from 'react'; 
 import styles from './Footer.module.css';
 
 const Footer = ({ }) => {
 
     return (
+        <>            
+            <section className={styles.Footer}>
 
-        <>
-            Footer
+                <section className={styles.FooterSocial}>
+                    Social Media
+                </section>
+
+                <hr className={styles.FooterContentSeparator}/>
+
+                <section className={styles.FooterLinks}>
+                    Strona główna Ciasteczka Kontakt Support FAQ
+                </section>
+
+                <section className={styles.FooterCopyright}>
+                    Copyright © 2022 OlympusMovie
+                </section>
+
+            </section>
+            
+            
         </>
-        
-        
-        
         );
 
 }
