3c838b1df528c57ed63f03dee7d68814d0d19ad5
Jakub Nowak <jakubszymonnowak@gmail.com>
2022-11-08 13:24:14 +0100

    OM-22 Implement the footer of the website # Improve footer and it's content

--- OlympusMovie-Frontend/src/components/Footer/Footer.tsx
+++ OlympusMovie-Frontend/src/components/Footer/Footer.tsx
@@ -1,4 +1,8 @@
 ﻿import React from 'react'; 
+import facebookLogo from '../../assets/facebooklogo.png';
+import discordLogo from '../../assets/discordlogo.png';
+import twitterlogo from '../../assets/twitterlogo.png';
+import instagramlogo from '../../assets/instagramlogo.png';
 import styles from './Footer.module.css';
 
 const Footer = ({ }) => {
@@ -8,13 +12,24 @@ const Footer = ({ }) => {
             <section className={styles.Footer}>
 
                 <section className={styles.FooterSocial}>
-                    Social Media
+                    <img src={facebookLogo} alt="facebook"></img>
+                    <img src={discordLogo} alt="discord"></img>
+                    <img src={instagramlogo} alt="instagram"></img>
+                    <img src={twitterlogo} alt="twitter"></img>
                 </section>
 
                 <hr className={styles.FooterContentSeparator}/>
 
                 <section className={styles.FooterLinks}>
-                    Strona główna Ciasteczka Kontakt Support FAQ
+                    <div className={styles.FooterLinksCenter}>
+                        |
+                    <a href=''>O nas</a>
+                    <a href=''>Ciasteczka</a>
+                    <a href=''>Kontakt</a>
+                    <a href=''>Support</a>
+                    <a href=''>FAQ</a>
+                        |
+                    </div>
                 </section>
 
                 <section className={styles.FooterCopyright}>
