51fd8d86100aa9639f4219c55feebdc10e0a9d39
Jakub Nowak <jakubszymonnowak@gmail.com>
2022-11-08 12:14:54 +0100

    OM-22 Implement the footer of the website # Move icons to the new created directory "assets"

--- OlympusMovie-Frontend/src/App.tsx
+++ OlympusMovie-Frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
 import './App.css';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
