8ba440313efce4c2b766b8ec117ef7b8ff8530ae
Dawid Polaczek <dkpolaczek@gmail.com>
2022-11-20 15:07:49 +0100

    OM-45 Create a microservice about people who participate in the creation of films # fix connection string

--- PersonService/Program.cs
+++ PersonService/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddSwaggerGen();
 var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
 var dbName = Environment.GetEnvironmentVariable("DB_NAME");
 var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
-var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword}";
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword};TrustServerCertificate=True";
 builder.Services.AddDbContext<PersonDbContext>(options => options.UseSqlServer(connectionString));
 
 // Register services
