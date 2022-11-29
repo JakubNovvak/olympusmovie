a5a179eab9707c568c805b5d00f3b064c81948ed
Marcelina Ziętal <38438425+marcelinazietal@users.noreply.github.com>
2022-11-30 00:40:59 +0100

    feature/OM-42 Create a microservice about users # implementation of basic functionality

---
+++ UserService/Model/User.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace UserService.Model
+{
+    [Table("user", Schema = "dbo")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; } = null!;
+
+        [Column("surname")]
+        public string Surname { get; set; } = null!;
+
+        [Column("nick_name")]
+        public string NickName { get; set; } = null!;
+
+        //[Column("watchig_movies")]
+        //public ICollection<int> WatchingMovies { get; set; } = null!;
+
+        //[Column("completed_movies")]
+        //public ICollection<int> CompletedMovies { get; set; } = null!;
+
+        //[Column("on_hold_movies")]
+        //public ICollection<int> OnHoldMovies { get; set; } = null!;
+
+        //[Column("dropped_movies")]
+        //public ICollection<int> DroppedMovies { get; set; } = null!;
+
+        //[Column("plan_to_watch_movies")]
+        //public ICollection<int> PlanToWatchMovies { get; set; } = null!;
+
+        //[Column("watchig")]
+        //public ICollection<int> WatchingSeries { get; set; } = null!;
+
+        //[Column("completed")]
+        //public ICollection<int> CompletedSeries { get; set; } = null!;
+
+        //[Column("on_hold")]
+        //public ICollection<int> OnHoldSeries { get; set; } = null!;
+
+        //[Column("dropped")]
+        //public ICollection<int> DroppedSeries { get; set; } = null!;
+
+        //[Column("plan_to_watch")]
+        //public ICollection<int> PlanToWatchSeries { get; set; } = null!;
+    }
+}
---
+++ UserService/Service/UserMapper.cs
@@ -0,0 +1,52 @@
+﻿using UserService.ApiModel;
+using UserService.Model;
+
+namespace UserService.Service
+{
+    public class UserMapper
+    {
+        public static UserDTO MapToDTO(User user)
+        {
+            return new UserDTO
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Surname = user.Surname,
+                NickName = user.NickName
+                //,
+                //WatchingMovies = new List<int>(user.WatchingMovies),
+                //CompletedMovies = new List<int>(user.CompletedMovies),
+                //OnHoldMovies = new List<int>(user.OnHoldMovies),
+                //DroppedMovies = new List<int>(user.DroppedMovies),
+                //PlanToWatchMovies = new List<int>(user.PlanToWatchMovies),
+                //WatchingSeries = new List<int>(user.WatchingSeries),
+                //CompletedSeries = new List<int>(user.CompletedSeries),
+                //OnHoldSeries = new List<int>(user.OnHoldSeries),
+                //DroppedSeries = new List<int>(user.DroppedSeries),
+                //PlanToWatchSeries = new List<int>(user.PlanToWatchSeries)
+            };
+        }
+
+        public static User MapToEntity(UserDTO userDTO)
+        {
+            return new User
+            {
+                Id = userDTO.Id,
+                Name = userDTO.Name,
+                Surname = userDTO.Surname,
+                NickName = userDTO.NickName
+                //,
+                //WatchingMovies = new List<int>(userDTO.WatchingMovies),
+                //CompletedMovies = new List<int>(userDTO.CompletedMovies),
+                //OnHoldMovies = new List<int>(userDTO.OnHoldMovies),
+                //DroppedMovies = new List<int>(userDTO.DroppedMovies),
+                //PlanToWatchMovies = new List<int>(userDTO.PlanToWatchMovies),
+                //WatchingSeries = new List<int>(userDTO.WatchingSeries),
+                //CompletedSeries = new List<int>(userDTO.CompletedSeries),
+                //OnHoldSeries = new List<int>(userDTO.OnHoldSeries),
+                //DroppedSeries = new List<int>(userDTO.DroppedSeries),
+                //PlanToWatchSeries = new List<int>(userDTO.PlanToWatchSeries)
+            };
+        }
+    }
+}
