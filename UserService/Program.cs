8719961ccf9ba2134b5ad3bcce6a00977ab4d72d
Dawid Polaczek <dkpolaczek@gmail.com>
2022-12-05 16:51:27 +0100

    OM-69 Cannot communicate with UserService through swagger # fix bug by adding TrustServerCertificate=True to connectionString

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
 var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
 var dbName = Environment.GetEnvironmentVariable("DB_NAME");
 var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
-var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword}";
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword};TrustServerCertificate=True";
 builder.Services.AddDbContext<UserDbContext>(options => options.UseSqlServer(connectionString));
 
 // Register services
