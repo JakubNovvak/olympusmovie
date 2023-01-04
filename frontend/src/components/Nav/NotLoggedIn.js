d6da011fda8d2975a6d8b4d5c26861665a6c7b69
Dawid Polaczek <dkpolaczek@gmail.com>
2023-01-04 02:48:32 +0100

    OM-71 Advance authentication service with refresh token, registration and revoke endpoints
    # fix bugs
    # add test button
    # add proper logout behavior

--- frontend/src/components/Nav/NotLoggedIn.js
+++ frontend/src/components/Nav/NotLoggedIn.js
@@ -3,19 +3,35 @@ import Box from "@mui/material/Box";
 import SearchBar from "./SearchBar";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export const NotLoggedIn = () => {
-    return (
-        <>
-            <Box marginLeft="auto">
-                <Link to="/Login">
-                    <Button color="inherit">Logowanie</Button>
-                </Link>
-                <Link to="/Register">
-                    <Button color="inherit">Rejestracja</Button>
-                </Link>
-            </Box>
-            <SearchBar />
-        </>
-    );
-}
+  return (
+    <>
+      <Box marginLeft="auto">
+        <Link to="/Login">
+          <Button color="inherit">Logowanie</Button>
+        </Link>
+        <Link to="/Register">
+          <Button color="inherit">Rejestracja</Button>
+        </Link>
+      </Box>
+      <Button
+        color="inherit"
+        onClick={() => {
+          axios
+            .post("/api/account/test", {
+              headers: { "Content-Type": "application/json" },
+            })
+            .then(
+              (response) => console.log(response),
+              (error) => console.log(error)
+            );
+        }}
+      >
+        Test
+      </Button>
+      <SearchBar />
+    </>
+  );
+};
